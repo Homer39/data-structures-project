cf27d02ec55f5f4fbf87fc0a0c74da1e15127594
Vlad <sapozhnikov.061098@mail.ru>
2023-04-21 15:45:00 +0300

    Реализованы классы, выполнены тесты

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,34 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+class TestNode(unittest.TestCase):
+
+    def test_node_init(self):
+        n1 = Node(5, None)
+        n2 = Node('a', n1)
+        self.assertEqual(n1.data, 5)
+        self.assertEqual(n2.data, 'a')
+        self.assertEqual(n2.next_node, n1)
+
+    def test_stack_push(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        stack.push('data3')
+        self.assertEqual(stack.top.data, 'data1')
+        self.assertEqual(stack.top.next_node.data, 'data2')
+        self.assertEqual(stack.top.next_node.next_node.data, 'data3')
+        self.assertEqual(stack.top.next_node.next_node.next_node, None)
+
+    def test_stack_error(self):
+        stack = Stack()
+        stack.push('data1')
+        with self.assertRaises(AttributeError):
+            stack.top.next_node.next_node.next_node.data
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
